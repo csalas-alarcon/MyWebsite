b935dd9f9944edb03d3d1aa78a6795fffa819560
csalas-alarcon <csalas0821@gmail.com>
2026-01-04 19:31:36 +0100

    Half done

---
+++ src/transmogrifier/target/release/deps/transmogrifier-fe03cdadd9a1da35.d
@@ -0,0 +1,5 @@
+/home/csalas/Desktop/MyWebsite/src/transmogrifier/target/release/deps/transmogrifier-fe03cdadd9a1da35.d: src/main.rs
+
+/home/csalas/Desktop/MyWebsite/src/transmogrifier/target/release/deps/transmogrifier-fe03cdadd9a1da35: src/main.rs
+
+src/main.rs:

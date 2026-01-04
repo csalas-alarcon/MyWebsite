3c89aefbd97bea93384c41985527b120b50d0cc6
csalas-alarcon <csalas0821@gmail.com>
2026-01-04 18:05:31 +0100

    New Transmogrifier (bin)

---
+++ src/transmogrifier/target/debug/deps/transmogrifier-d718cfed42f7b569.d
@@ -0,0 +1,5 @@
+/home/csalas/Desktop/MyWebsite/src/transmogrifier/target/debug/deps/transmogrifier-d718cfed42f7b569.d: src/main.rs
+
+/home/csalas/Desktop/MyWebsite/src/transmogrifier/target/debug/deps/transmogrifier-d718cfed42f7b569: src/main.rs
+
+src/main.rs:
---
+++ src/transmogrifier/target/debug/transmogrifier.d
@@ -0,0 +1,1 @@
+/home/csalas/Desktop/MyWebsite/src/transmogrifier/target/debug/transmogrifier: /home/csalas/Desktop/MyWebsite/src/transmogrifier/src/main.rs
